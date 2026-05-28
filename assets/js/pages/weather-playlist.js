b30150d1bfe62062edfa56521405d8d8c7cb66c2
Ppakso <apfkzl9060@gmail.com>
2026-05-28 17:20:55 +0900

    Feat: 날씨 플레이리스트 파일 생성

---
+++ assets/js/pages/weather-playlist.js
@@ -0,0 +1,12 @@
+import { renderCommonLayout } from '../layout/commonLayout.js';
+
+// =========================
+// 초기 실행
+// =========================
+renderCommonLayout();
+
+function initWeatherPlaylist() {
+  console.log('Weather Playlist page loaded');
+}
+
+initWeatherPlaylist();
